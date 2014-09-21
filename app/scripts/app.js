394d1968ee7287095a2fb4b7ff6bdac8b24df08a
kat2n <daisuke.katsumata@dragon.jp>
2014-09-21 14:57:22 +0900

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name colombiaApp
+ * @description
+ * # colombiaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('colombiaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name colombiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the colombiaApp
+ */
+angular.module('colombiaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
