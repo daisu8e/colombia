161c163cb45b5452ccb3e25d02948e4c56d4d410
kat2n <daisuke.katsumata@dragon.jp>
2014-09-29 16:36:23 +0900

    add pv counter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,96 @@
  * Controller of the colombiaApp
  */
 angular.module('colombiaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+
+    $scope.dailyPv = 0;
+
+
+    (function updateDailyPv() {
+      var when = Date.create().format('{yyyy}-{MM}-{dd}');
+      $http.get('http://dev.api.apimail.jp/api/1.0/pv/account/kat2n/daily/' + when)
+        .success(function (data) {
+          $scope.dailyPv = data.count;
+          $timeout(updateDailyPv, 5000);
+        })
+      ;
+    }());
+
+
+
+    Highcharts.setOptions({
+      global: {
+        useUTC: false
+      }
+    });
+
+    $('#container').highcharts({
+      chart: {
+        type: 'spline',
+        animation: Highcharts.svg,
+        marginRight: 10,
+        events: {
+          load: function () {
+            var series = this.series[0];
+            setInterval(function () {
+              var
+                x = (new Date()).getTime(),
+                y = parseFloat($scope.dailyPv)
+              ;
+              series.addPoint([x, y], true, true);
+            }, 1000);
+          }
+        }
+      },
+      title: {
+        text: 'PV Real-Time Report'
+      },
+      xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+      },
+      yAxis: {
+        title: {
+          text: 'PV'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.series.name + '</b><br/>' +
+          Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+          Highcharts.numberFormat(this.y, 2);
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        name: 'Random data',
+        data: (function () {
+          var
+            data = [],
+            time = (new Date()).getTime(),
+            i
+          ;
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: 0
+            });
+          }
+          return data;
+        }())
+      }]
+    });
+
+
+  })
+;
